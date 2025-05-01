ff661e8fd4b16dd11d931bb0a8d69c6ac82bbce6
117Ustim <ustik72@gmail.com>
2025-05-02 02:20:06 +0300

    4

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
   <React.StrictMode>
-      <BrowserRouter basename="/wedding">
+      <BrowserRouter>
        <App storage={storage} />
     
       </BrowserRouter>
